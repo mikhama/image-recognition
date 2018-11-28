f78456630b738166a44837faaec22d0e1fddad11
Dzmitry Tsebruk <mikhama@tut.by>
2018-11-28 21:14:48 +0300

    refact: rebuild application for usage with only one button

--- src/client/index.js
+++ src/client/index.js
@@ -1,39 +1,47 @@
 const imageSelector = document.querySelector('#image-selector');
 const selectedImage = document.querySelector('#selected-image');
 const predictionList = document.querySelector('#prediction-list');
-const progressText = document.querySelector('#progress-text');
-const predictionButton = document.querySelector('#prediction-button');
+const progressStatus = document.querySelector('#progress-status');
+const progress = document.querySelector('#progress');
 
 const loadModel = async () => {
   const model = await tf.loadModel('/model/model.json');
 
-  progressText.innerHTML = 'Model is loaded';
+  progressStatus.innerHTML = 'Idle';
+  progress.classList.remove('-loading');
 
   return model;
 };
 
 const loadedModel = loadModel();
 
-imageSelector.addEventListener('input', () => {
-  const reader = new FileReader();
+const selectImage = async () => {
+  progress.classList.add('-recognition');
+  progressStatus.innerHTML = 'Recognition...';
 
-  reader.onload = () => {
-    const imgUrl = reader.result;
-    selectedImage.setAttribute('src', imgUrl);
+  return new Promise((resolve) => {
+    const reader = new FileReader();
 
-    predictionList.innerHTML = '';
-  };
+    reader.onload = () => {
+      const imgUrl = reader.result;
+      selectedImage.setAttribute('src', imgUrl);
 
-  const file = imageSelector.files[0];
-  reader.readAsDataURL(file);
-});
+      predictionList.innerHTML = '';
 
-predictionButton.addEventListener('click', async () => {
+      resolve(reader);
+    };
+
+    const file = imageSelector.files[0];
+    reader.readAsDataURL(file);
+  });
+};
+
+
+const predict = async () => {
   const model = await loadedModel;
-  const image = document.querySelector('#selected-image');
 
   const offset = tf.scalar(127.5);
-  const tensor = tf.fromPixels(image)
+  const tensor = tf.fromPixels(selectedImage)
     .resizeNearestNeighbor([224, 224])
     .toFloat()
     .sub(offset)
@@ -41,14 +49,23 @@ predictionButton.addEventListener('click', async () => {
     .expandDims();
 
   const prediction = await model.predict(tensor).data();
+
   const top5 = Array.from(prediction)
-    .map((prob, idx) => ({ probability: prob, className: modelDescription[idx] }))
+    .map((prob, idx) => ({ probability: prob, name: modelDescription[idx] }))
     .sort((a, b) => b.probability - a.probability)
     .slice(0, 5);
 
   let elements = '';
-  top5.forEach(({ probability, className }) => {
-    elements += `<li><span class="prediction-item-name">${className}</span> <span class="prediction-item-probability">${probability}</span></li>`;
+  top5.forEach(({ probability, name }) => {
+    const percent = Number(probability * 100).toFixed(2);
+    elements += `<li class="prediction-list-item"><span class="prediction-item-name">${name}</span> <span class="prediction-item-probability">${percent}%</span></li>`;
   });
   predictionList.innerHTML = elements;
+
+  progress.classList.remove('-recognition');
+  progressStatus.innerHTML = 'Idle';
+};
+
+imageSelector.addEventListener('input', async () => {
+  selectImage().then(() => predict());
 });
